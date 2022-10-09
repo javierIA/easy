512dc88d659d53e9be0594be75d128fc10a7e48a
Javier Flores <81644684+javierIA@users.noreply.github.com>
2022-10-09 00:35:16 -0600

    aaa

--- db/db/db.py
+++ db/db/db.py
@@ -3,11 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import urllib
 from sqlalchemy import create_engine
+from dotenv import dotenv_values, load_dotenv
+import os
 
-server = 'spacebot.database.windows.net' # to specify an alternate port
-database = 'spacebot' 
-username = 'Javier' 
-password = 'Sp4c3bot.2022@'
+load_dotenv()
+
+server = os.getenv("HOST")
+database = os.getenv("DATABASE")
+username = os.getenv("USER")
+password = os.getenv("PASSWORD")
 params = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};SERVER=" + server + ";DATABASE=" + database + ";UID=" + username + ";PWD=" + password)
 engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 Session = sessionmaker(bind=engine, autoflush=True, autocommit=True)
