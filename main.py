59f70e7c6020bb77d155f6872a10e005d0da82cb
Javier Flores <81644684+javierIA@users.noreply.github.com>
2022-10-09 17:27:25 -0600

    io

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import uvicorn
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", log_level="info",reload=True)
+from app import app
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+config = Config()
+config.bind = [os.getenv("BIND")+":"+os.getenv("PORT")]
+config.loglevel = "info"
+config.use_reloader = True
+config.reload_dirs = ["app"]
+config.worker_class = "trio"
+asyncio.run(serve(app, config))
